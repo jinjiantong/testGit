90bef648b10abeaecf8014a646de6b58f0d7a24b
kim <>
2020-12-30 00:31:02 +0800

    le:   testgit/testgit.xcodeproj/xcuserdata/kim.xcuserdatad/xcschemes/xcschememanagement.plist
    工程初始化

---
+++ testgit/Shared/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  Shared
+//
+//  Created by kim on 2020/12/30.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
