fd67f086be992f1ac67a3dee3b881cb3cbae5e12
kim <>
2020-12-30 00:51:00 +0800

    asdf

--- testgit/Shared/ContentView.swift
+++ testgit/Shared/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!asdfaasdfasdfasdfads")
+        Text("Hello, world!asdfaasdfasdfasdasdfasdfasdfasdfasfasfasdfads")
             .padding()
     }
 }
